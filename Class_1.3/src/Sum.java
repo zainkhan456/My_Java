7718746d08e208bbd33ecea25ef74dc97707eff8
zainkhan456 <zainulislamk456@gmail.com>
2025-12-05 07:06:31 +0500

    GUI Program

---
+++ Class_1.3/src/Sum.java
@@ -0,0 +1,14 @@
+//GUI Input And Output Program
+import javax.swing.JOptionPane;
+public class Sum{
+    public static void main(String[] args){
+        int a,b,sum;
+        String value;
+        value = JOptionPane.showInputDialog("Enter your first number: ");
+        a = Integer.parseInt(value);
+        value = JOptionPane.showInputDialog("Enter your second number: ");
+        b = Integer.parseInt(value);
+        sum = a + b;
+        JOptionPane.showMessageDialog(null,"The sum of "+a +" and "+b +" is "+sum);
+    }
+}
