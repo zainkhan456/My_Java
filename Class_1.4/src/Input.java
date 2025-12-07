9e017e5658e8f2766e23a47b867845120a78df79
zainkhan456 <zainulislamk456@gmail.com>
2025-12-07 08:44:34 +0500

    Addition program using console for read and write

---
+++ Class_1.4/src/Input.java
@@ -0,0 +1,15 @@
+//Console Input And Output Program using scanner class
+//Reading integer value from user
+import java.util.Scanner;
+public class Input{
+    public static void main(String[] args){
+        int a,b,sum;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter Your Fisrt number: ");
+        a = input.nextInt();
+        System.out.println("Enter Your Second Number: ");
+        b = input.nextInt();
+        sum = a + b;
+        System.out.println("The sum of "+a+" and "+b+" is "+sum);
+    }
+}
