8e4315148e4d4e45db6866f0a33c484429d1e468
zainkhan456 <zainulislamk456@gmail.com>
2025-12-20 16:56:29 +0500

    Dynamic method

---
+++ Class_1.9/src/Test.java
@@ -0,0 +1,41 @@
+//Dynamic method dispatch
+//It mean that which overrided method should be call in different sitiuation or conditions
+class A{
+    public A(){
+        
+    }
+    public void callMe(){
+        System.out.println("Inside the callMe() method of class A");
+    }
+}
+class B extends A{
+    public B(){
+        
+    }
+    public void callMe(){
+        System.out.println("inside the callMe() method of class B");
+    }
+}
+class C extends A{
+    public C(){
+        
+    }
+    public void callMe(){
+        System.out.println("Inside the callMe() method of class C");
+    }
+}
+public class Test{
+    public static void main(String[] args){
+        A a = new A();
+        B b = new B();
+        C c = new C();
+        A ref;
+        ref = a;
+        a.callMe();
+        ref = b;
+        b.callMe();
+        ref = c;
+        c.callMe();
+        
+    }
+}
